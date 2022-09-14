6ad190f5367641ef6fa6de31d5e8be891de49ce8
AlexandrVino <64733647+ded-masay@users.noreply.github.com>
2022-09-14 14:20:51 +0500

    rename files

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+import logging
 import os
 from time import sleep
 
 from disk.api.__main__ import main
 
+log = logging.getLogger(__name__)
+
 if __name__ == '__main__':
     # Ждем пока создастся и запустится контейнер бд в докере
     # (чтобы избежать лишней ошибки, т.к. без sleep бэк один раз упадет при попытке коннекта,
     # потом перезапустится и все будет ок)
-    # sleep(5)
+    sleep(5)
 
     # применяем миграции к базе данных
-    # os.system('python disk/db/__main__.py upgrade head')
+
+    log.info('Starting migrations script')
+    os.system('python disk/db/__main__.py upgrade head')
+    log.info('Ending of migrations script')
 
     # запускаем бэк
     main()
